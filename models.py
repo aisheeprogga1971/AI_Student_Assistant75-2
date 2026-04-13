98aa688ac9f7443fbd0e8ba59b7a488fc11d2761
aisheeprogga1971 <aisheeprogga1971@gmail.com>
2026-04-14 01:29:21 +0600

    Add files via upload

---
+++ models.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+class User(BaseModel):
+    username: str
+    password: str
+
+class Question(BaseModel):
+    text: str
+
+class Writing(BaseModel):
+    text: str
+
+
+class Question(BaseModel):
+    text: str
+app = FastAPI()
+@app.post("/ask")
+def ask(q: Question):
+    print("Received:", q.text)
+    return {"answer": "OK backend working"}
